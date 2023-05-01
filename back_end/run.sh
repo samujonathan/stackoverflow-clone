3122af84bafe94eb369a83ef71cd8f85133f3085
samujonathan <87077500+samujonathan@users.noreply.github.com>
2023-05-01 09:15:41 +0530

    Delete run.sh

--- back_end/run.sh
+++
@@ -1,2 +0,0 @@
-PORT=5000 HOST='localhost' USERNAME='jonathan' PASSWORD='Relin!111' GOOGLE_CLIENT_ID='622679705499-d4g1vifkue2rtsrmil2318lg61k8nrt2.apps.googleusercontent.com' JWT_SECRET='myScret' npx nodemon ./bin/www
-
